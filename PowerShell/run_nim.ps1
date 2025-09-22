5670b224ad4f5e3ad114f63a3b14b686a07cd690
Evan Martinez <147688099+Nave55@users.noreply.github.com>
2025-09-22 00:26:59 -0600

    Update run_nim.ps1

--- PowerShell/run_nim.ps1
+++ PowerShell/run_nim.ps1
@@ -2,12 +2,17 @@ param(
 [string]$Name,
 [switch]$Verbose,
 [switch]$Release,
+[switch]$Build,
 [switch]$Help
 )
 
 $exe_loc = "output\$Name.exe"
-$flags = @("c", "--verbosity:0", "--hints:off", "-o=output/$Name.exe", "-r")
+$flags = @("c", "--verbosity:0", "--hints:off", "-o=output/$Name.exe")
 $Name += ".nim"
+
+if (!$Build) {
+	$flags += "-r"
+}
 
 if ($Verbose) {
 	$flags = @("c", "-o=output/$Name.exe", "-r")
@@ -19,7 +24,7 @@ if ($Release) {
 
 cls
 if ($Help) { 
-	Write-Host "Options: -Name, -Verbose, -Release" 
+	Write-Host "Options: -Name, -Verbose, -Release, -Build" 
 } else {
 	Write-Host "nim $flags $Name"
 	nim @flags $Name
