f9d3b875d4fef519dd69cef8a81f943773e98b82
Evan Martinez <147688099+Nave55@users.noreply.github.com>
2025-09-25 12:38:31 -0600

    Update run_cpp.ps1

--- PowerShell/run_cpp.ps1
+++ PowerShell/run_cpp.ps1
@@ -1,5 +1,6 @@
 param(
 [string]$Name,
+[string]$Out,
 [switch]$Release,
 [switch]$G3,
 [switch]$Build,
@@ -8,10 +9,19 @@ param(
 )
 
 $cpp_loc = ".\$Name.cpp"
-$exe_loc = "output\$Name.exe"
+$exe_loc = "$Out\$Name.exe"
 $cpp_vers = "-std=gnu++2c"
 $flags = @("-O0", "-Wextra", "-Wall")
 $debug_mode = "-DDEBUG"
+
+if ($Out -ne "") {
+	if (-not (Test-Path -Path $Out -PathType Container)) {
+		New-Item -Path $Out -ItemType Directory
+	}
+} else {
+	$exe_loc = "$Name.exe"
+}
+
 
 if ($Release) {
     $debug_mode = "-DRELEASE"
@@ -26,5 +36,5 @@ if ($Help) {
 } else {
 	if ($Verbose) { Write-Host "clang $cpp_loc $cpp_vers $flags $debug_mode -o $exe_loc" }
     clang $cpp_loc $cpp_vers @flags $debug_mode -o $exe_loc
-    if (!$Build) { . $exe_loc }
+    if (!$Build) { & ".\$($exe_loc)" }
 }
