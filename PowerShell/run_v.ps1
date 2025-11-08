b649d5655fe57cf566198c2b8aa38ebf2cb623ed
Evan Martinez <147688099+Nave55@users.noreply.github.com>
2025-11-07 22:55:19 -0700

    Update run_v.ps1

--- PowerShell/run_v.ps1
+++ PowerShell/run_v.ps1
@@ -1,30 +1,48 @@
 param(
 [string]$Name,
+[string]$Dir,
+[string]$Out,
 [switch]$Release,
 [switch]$G,
-[switch]$Show,
+[switch]$Verbose,
 [switch]$Help
 )
 
-$exe_loc = "output\$Name.exe"
-$flags = @()
-
-if ($G) {
-    $flags += "-g"
+if ($Out -ne "") {
+	if (-not (Test-Path -Path $Out -PathType Container)) {
+		New-Item -Path $Out -ItemType Directory
+	}
 }
 
+$flags = @()
+$exe_loc = "$Out\$Name.exe"
+$name_path = "$Name"
+
+if ($Dir -ne "") { $name_path = "$Dir\$Name" }
+if ($Out -eq "") { $exe_loc = "$Name.exe" }
+if ($G) { $flags += "-g" }
+
+
 if ($Release) {
-    $flags = @("-prod", "-o", $exe_loc, "crun")
+    if ($Out -ne "") {
+      $flags = @("-prod", "-o", $exe_loc, "crun")
+    } else {
+      $flags = @("-prod", "run")
+    }
 } else {
-    $flags += "run"
+    if ($Out -ne "") {
+      $flags = @("-o", $exe_loc, "crun")
+    } else {
+      $flags += "run"
+    }
 }
 
 cls
-if ($Help) { 
-	Write-Host "Options: -Name, -Release, -G, -Show" 
+if ($Help) {
+	Write-Host "Options: -Name, -Dir, -Out, -Release, -G, -Verbose"
 } else {
-	if ($Show) {
+	if ($Verbose) {
 		Write-Host "v $flags $Name"
 	}
-    v @flags $Name
+    v @flags $name_path
 }
