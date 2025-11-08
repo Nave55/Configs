680cc2407fe465217f12e3c084b3b527e7a43114
Evan Martinez <147688099+Nave55@users.noreply.github.com>
2025-11-08 11:16:01 -0700

    Update run_v.ps1

--- PowerShell/run_v.ps1
+++ PowerShell/run_v.ps1
@@ -2,6 +2,7 @@ param(
 [string]$Name,
 [string]$Dir,
 [string]$Out,
+[string]$CC,
 [switch]$Release,
 [switch]$G,
 [switch]$Verbose,
@@ -14,32 +15,37 @@ if ($Out -ne "") {
 	}
 }
 
-$flags = @()
+if ($CC -ne "clang" -and $CC -ne "gcc" -and $CC -ne "msvc" -and $CC -ne "tcc") {
+  $CC = "tcc"
+}
+
+$flags = @("-cc", $CC)
+if ($G) { $flags += "-g" }
 $exe_loc = "$Out\$Name.exe"
 $name_path = "$Name"
 
 if ($Dir -ne "") { $name_path = "$Dir\$Name" }
 if ($Out -eq "") { $exe_loc = "$Name.exe" }
-if ($G) { $flags += "-g" }
 
 
 if ($Release) {
     if ($Out -ne "") {
-      $flags = @("-prod", "-o", $exe_loc, "crun")
+      $flags += @("-prod", "-o", $exe_loc, "crun")
     } else {
-      $flags = @("-prod", "run")
+      $flags += @("-prod", "run")
     }
 } else {
     if ($Out -ne "") {
-      $flags = @("-o", $exe_loc, "crun")
+      $flags += @("-o", $exe_loc, "crun")
     } else {
       $flags += "run"
     }
 }
 
+
 cls
 if ($Help) {
-	Write-Host "Options: -Name, -Dir, -Out, -Release, -G, -Verbose"
+	Write-Host "Options: -Name, -Dir, -Out, -Release, -G, -Verbose, -CC"
 } else {
 	if ($Verbose) {
 		Write-Host "v $flags $Name"
