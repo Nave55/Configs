fdaaad89315d1c0feb69cb50239ce498bd0fd3b2
Evan Martinez <147688099+Nave55@users.noreply.github.com>
2026-05-14 21:51:22 -0600

    Update run_cpp.ps1

--- PowerShell/run_cpp.ps1
+++ PowerShell/run_cpp.ps1
@@ -1,11 +1,11 @@
 param(
-[string]$Name,
-[string]$Out,
-[switch]$Release,
-[switch]$G3,
-[switch]$Build,
-[switch]$Help,
-[switch]$Verbose
+  [string]$Name,
+  [string]$Out,
+  [switch]$Release,
+  [switch]$G3,
+  [switch]$Build,
+  [switch]$Help,
+  [switch]$Verbose
 )
 
 $cpp_loc = ".\$Name.cpp"
@@ -15,26 +15,32 @@ $flags = @("-O0", "-Wextra", "-Wall")
 $debug_mode = "-DDEBUG"
 
 if ($Out -ne "") {
-	if (-not (Test-Path -Path $Out -PathType Container)) {
-		New-Item -Path $Out -ItemType Directory
-	}
+  if (-not (Test-Path -Path $Out -PathType Container)) {
+    New-Item -Path $Out -ItemType Directory
+  }
 } else {
-	$exe_loc = "$Name.exe"
+  $exe_loc = "$Name.exe"
 }
-
 
 if ($Release) {
-    $debug_mode = "-DRELEASE"
-    $flags = @("-O3")
+  $debug_mode = "-DRELEASE"
+  $flags = @("-O3")
 }
 
-if ($G3) { $flags += "-g3" }
+if ($G3){ 
+    $flags += "-g3" 
+}
 
-cls
+Clear-Host
 if ($Help) {
-    Write-Host "Options: -Name, -Release, -Build, -G3, -Verbose" 
+  Write-Host "Options: -Name, -Release, -Build, -G3, -Verbose"
 } else {
-	if ($Verbose) { Write-Host "clang $cpp_loc $cpp_vers $flags $debug_mode -o $exe_loc" }
-    clang $cpp_loc $cpp_vers @flags $debug_mode -o $exe_loc
-    if (!$Build) { & ".\$($exe_loc)" }
+  if ($Verbose) { 
+    Write-Host "clang $cpp_loc $cpp_vers $flags $debug_mode -o $exe_loc" 
+  }
+  
+  g++ $cpp_loc $cpp_vers @flags $debug_mode -o $exe_loc 
+  if (!$Build) {
+    & ".\$($exe_loc)" 
+  }
 }
